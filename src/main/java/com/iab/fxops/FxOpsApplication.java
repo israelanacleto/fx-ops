4c9b730da79a2277fce29dcbbe41252b817e9e13
Israel Igarashi <89955427+israelanacleto@users.noreply.github.com>
2026-06-12 14:20:01 -0300

    bloco 1: setup do projeto, application.yaml e endpoint /health

---
+++ src/main/java/com/iab/fxops/FxOpsApplication.java
@@ -0,0 +1,13 @@
+package com.iab.fxops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iab/fxops/FxOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iab.fxops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
